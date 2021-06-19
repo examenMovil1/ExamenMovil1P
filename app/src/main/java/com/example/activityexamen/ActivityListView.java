aaf4e4f515137cb42f69d3cb611fd3c0bdc84883
Stephany2000 <calixstephany2000@gmail.com>
2021-06-18 18:44:57 -0600

    agregar proyecto

---
+++ app/src/main/java/com/example/activityexamen/ActivityListView.java
@@ -0,0 +1,131 @@
+package com.example.activityexamen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import com.example.activityexamen.contactos.Contactos;
+import com.example.activityexamen.transacciones.Transacciones;
+
+import java.util.ArrayList;
+
+public class ActivityListView extends AppCompatActivity {
+
+    //Variables globales de la actividad
+    SQLiteConexion conexion;
+    ListView listacontactos;
+    EditText id;
+
+    ArrayList<Contactos> lista;
+    ArrayList<String> ArregloContactos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        conexion= new SQLiteConexion(this, Transacciones.NameDataBase, null, 1);
+        listacontactos= (ListView) findViewById(R.id.listaContactos);
+        id = (EditText) findViewById(R.id.txtcid);
+        Button btnregresar = (Button)findViewById(R.id.btnregresar);
+
+        Button btneliminar = (Button)findViewById(R.id.btneliminar);
+
+        btneliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eliminar();
+
+            }
+        });
+
+        btnregresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+        ObtenerListaContactos();
+
+        ArrayAdapter adp = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ArregloContactos);
+        listacontactos.setAdapter(adp);
+
+        id.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adp.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+    }
+
+    private void eliminar() {
+        SQLiteDatabase db = conexion.getWritableDatabase();
+        String []  params = {id.getText().toString()};
+        String wherecond = Transacciones.id + "=?";
+        db.delete(Transacciones.tablacontactos, wherecond, params);
+        Toast.makeText(getApplicationContext(),"Dato eliminado", Toast.LENGTH_LONG).show();
+
+    }
+
+
+
+    private void ObtenerListaContactos() {
+        SQLiteDatabase db = conexion.getReadableDatabase();
+        Contactos listContactos= null;
+        lista = new ArrayList<Contactos>();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM " + Transacciones.tablacontactos, null);
+
+        while (cursor.moveToNext()){
+            listContactos = new Contactos();
+            listContactos.setId(cursor.getInt(0));
+            listContactos.setNombre(cursor.getString(2));
+            listContactos.setTelefono(cursor.getString(3));
+
+            lista.add(listContactos);
+
+        }
+        cursor.close();
+        fillList();
+
+    }
+    private void fillList() {
+
+        ArregloContactos = new ArrayList<String>();
+
+        for (int i = 0;  i < lista.size(); i++){
+
+            ArregloContactos.add(lista.get(i).getId() + " | "
+                    +lista.get(i).getNombre() + " | "
+                    +lista.get(i).getTelefono());
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/activityexamen/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.activityexamen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.activityexamen.transacciones.Transacciones;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText pais,nombre,telefono,nota;
+    Spinner comboPais;
+
+    public String paises;
+    public String cptxt;
+    public String cpaisesn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        nombre = (EditText)findViewById(R.id.txtnombre);
+        telefono = (EditText)findViewById(R.id.txttelefono);
+        nota= (EditText)findViewById(R.id.txtnota);
+        comboPais =(Spinner)findViewById(R.id.comboPais);
+
+        ArrayAdapter<CharSequence> adp = ArrayAdapter.createFromResource(this,R.array.combo_pais, android.R.layout.simple_spinner_item);
+        comboPais.setAdapter(adp);
+
+
+        Button btn = (Button)findViewById(R.id.btnguardar);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(validar()){
+                    AgregarContacto();
+                }
+
+            }
+        });
+
+        Button btnlista = (Button)findViewById(R.id.btnlista);
+        btnlista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ActivityListView.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+    public boolean validar(){
+        boolean retorno= true;
+
+        String nom= nombre.getText().toString();
+        String tel= telefono.getText().toString();
+        String nt= nota.getText().toString();
+
+        if(nom.isEmpty()){
+            nombre.setError("Campo nombre no puede quedar vacio");
+            retorno = false;
+        }
+        if(tel.isEmpty()){
+            telefono.setError("Campo telefono no puede quedar vacio");
+            retorno = false;
+        }
+        if(nt.isEmpty()){
+            nota.setError("Campo nota no puede quedar vacio");
+            retorno = false;
+        }
+
+        return retorno;
+
+    }
+
+    public void recortar() {
+        paises = comboPais.getSelectedItem().toString();
+        cptxt = paises.substring(0, paises.length() - 5);
+        cpaisesn = paises.substring(paises.length() - 4, paises.length() - 1);
+    }
+
+    private void AgregarContacto() {
+        recortar();
+
+        SQLiteConexion conexion = new SQLiteConexion(   this,Transacciones.NameDataBase, null, 1);
+        SQLiteDatabase db = conexion.getWritableDatabase();
+        ContentValues valores = new ContentValues();
+
+        valores.put(Transacciones.pais, cptxt );
+        valores.put(Transacciones.nombre, nombre.getText().toString());
+        valores.put(Transacciones.telefono,cpaisesn + telefono.getText().toString());
+        valores.put(Transacciones.nota, nota.getText().toString());
+
+        Long resultado = db.insert(Transacciones.tablacontactos, Transacciones.id, valores);
+        Toast.makeText(getApplicationContext(), "Registro Ingresado:" + resultado.toString(),Toast.LENGTH_LONG).show();
+
+        db.close();
+        ClearScreen();
+    }
+    private void ClearScreen()
+    {
+        comboPais.setSelection(0);
+        nombre.setText("");
+        telefono.setText("");
+        nota.setText("");
+    }
+
+}
